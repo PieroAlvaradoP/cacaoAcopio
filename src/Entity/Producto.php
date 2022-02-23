3e7dd94b9e57d6f482772934af2c8c60ed376d6c
marcelocoronado2000 <marcelocoronado2000@hotmail.com>
2022-02-23 02:43:59 -0500

    Add module "Producto"

---
+++ src/Entity/Producto.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Pidia\Apps\Demo\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+use Pidia\Apps\Demo\Entity\Traits\EntityTrait;
+use Pidia\Apps\Demo\Repository\ProductoRepository;
+
+#[ORM\Entity(repositoryClass: ProductoRepository::class)]
+#[HasLifecycleCallbacks]
+class Producto
+{
+    use EntityTrait;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 20)]
+    private $nombre;
+
+    #[ORM\Column(type: 'string', length: 15)]
+    private $alias;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getAlias(): ?string
+    {
+        return $this->alias;
+    }
+
+    public function setAlias(string $alias): self
+    {
+        $this->alias = $alias;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getAlias().'-'.$this->getNombre();
+    }
+}
